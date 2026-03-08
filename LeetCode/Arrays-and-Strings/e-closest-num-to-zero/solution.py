75d5d6ab01c49c8df51e988afa9ea8ac34c9b2d5
Keaton Nordby <keatonnordby4@gmail.com>
2026-03-08 16:18:58 -0500

    lc

---
+++ LeetCode/Arrays-and-Strings/e-closest-num-to-zero/solution.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def findClosestNumber(self, nums):
+
+        if not nums:
+            return None
+
+        closest = nums[0]
+
+        for num in nums:
+            if abs(num) < abs(closest) or (abs(num) == abs(closest) and num > closest):
+                closest = num
+
+        return closest
+    
+    
+s = Solution()
+print(s.findClosestNumber([-4, -2, 1, 4, 8]))  # Output: 1
+
+
+# Time Complexity - O(n)
+# Space Complexity - O(1)
