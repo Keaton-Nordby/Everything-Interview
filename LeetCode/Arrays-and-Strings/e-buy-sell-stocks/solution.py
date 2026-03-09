6d41a08117786b38f6bb8faae74c32fc3977795a
Keaton Nordby <keatonnordby4@gmail.com>
2026-03-09 17:37:08 -0500

    solution

--- LeetCode/Arrays-and-Strings/e-buy-sell-stocks/solution.py
+++ LeetCode/Arrays-and-Strings/e-buy-sell-stocks/solution.py
@@ -1,9 +1,18 @@
 class Solution(object):
     def maxProfit(self, prices):
-        """
-        :type prices: List[int]
-        :rtype: int
-        """
+        
+        l, r = 0, 1
+        max_profit = 0
+        
+        while r < len(prices):
+            if prices[r] > prices[l]:
+                profit = prices[r] - prices[l]
+                max_profit = max(profit, max_profit)
+            else:
+                l = r
+            r += 1
+            
+        return max_profit
         
        
 prices = [7,1,5,3,6,4]
