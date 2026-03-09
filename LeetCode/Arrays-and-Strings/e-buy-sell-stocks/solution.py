25df0d8fdf91e3425a577727d1aa3c1138e63429
Keaton Nordby <keatonnordby4@gmail.com>
2026-03-09 17:29:49 -0500

    solution

---
+++ LeetCode/Arrays-and-Strings/e-buy-sell-stocks/solution.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        
+       
+prices = [7,1,5,3,6,4]
+s = Solution()
+print(s.maxProfit(prices)) # output should be 5
