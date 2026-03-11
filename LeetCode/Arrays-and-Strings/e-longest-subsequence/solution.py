0a35fb9a934c2ce2f6f827a803de520f89fee014
Keaton Nordby <keatonnordby4@gmail.com>
2026-03-10 19:56:06 -0500

    solution

--- LeetCode/Arrays-and-Strings/e-longest-subsequence/solution.py
+++ LeetCode/Arrays-and-Strings/e-longest-subsequence/solution.py
@@ -1,11 +1,26 @@
 class Solution(object):
     def longestCommonPrefix(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: str
-        """
+        
+        
+        min_length = float("inf")
+        
+        for s in strs:
+            if len(s) < min_length:
+                min_length = len(s)
+        
+        i = 0
+        while i < min_length:
+            for s in strs:
+                if s[i] != strs[0][i]:
+                    return s[:i]
+            i += 1
+        
+        return s[:1]        
         
 
 strs = ["flower","flow","flight"]      
 s = Solution()
 print(s.longestCommonPrefix(strs))
+
+# Time - O(n * m)
+# Space - O(1)
