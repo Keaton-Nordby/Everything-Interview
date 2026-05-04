cb52e68f2a8f9e75053270428c906c552fb02bd0
Keaton Nordby <keatonnordby4@gmail.com>
2026-05-03 19:28:40 -0500

    time stamps added

--- MISC/logger.py
+++ MISC/logger.py
@@ -1,26 +1,30 @@
 # Log levels (INFO, ERROR, DEBUG)
 # Store logs in memory
 # Filter logs
-
+from datetime import datetime
 
 logs = []
 
 
+# add log to log list
 def add_log(logs, level, message):
     log = {
         "level" : level,
-        "message" : message
+        "message" : message,
+        "timestamp" : datetime.now()
     }
     logs.append(log)
     
-    
+
+# filtering logs by level 
 def filter_logs(logs, level):
     return [log for log in logs if log["level"] == level]
 
 
+# printing logs
 def print_logs(logs):
     for log in logs:
-        print(f"[{log['level']}] {log['message']}")
+        print(f"[{log['timestamp']}] [{log['level']}] {log['message']}")
 
 
 add_log(logs, "INFO", "User logged in")
